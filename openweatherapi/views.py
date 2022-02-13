f23b62e29a8acd6528c3303c3a47a4ab6de23f8c
TTs <titus.brasov@gmail.com>
2022-02-13 15:56:57 +0200

    Open Weather API update

--- openweatherapi/views.py
+++ openweatherapi/views.py
@@ -17,21 +17,23 @@ def openweather(request):
         api = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + openweather_api_key
         
         # Convert the response into a python dictionary
-        list_of_data = requests.get(api).json()
+        retrieved_data = requests.get(api).json()
         
         try:
             # Manipulate the data
             data = {
-                "country_code": str(list_of_data['sys']['country']),
-                "latitude": str(list_of_data['coord']['lat']) + '°',
-                "longitude": str(list_of_data['coord']['lon']) + '°',
-                "temp": str(list_of_data['main']['temp']) + ' °C',
-                "pressure": str(list_of_data['main']['pressure']) + " hPa",
-                "humidity": str(list_of_data['main']['humidity']) + '%',
-                'main': str(list_of_data['weather'][0]['main']),
-                'description': str(list_of_data['weather'][0]['description']),
-                'icon': list_of_data['weather'][0]['icon'],
+                "city": str(retrieved_data['name']),
+                "country_code": str(retrieved_data['sys']['country']),
+                "latitude": str(retrieved_data['coord']['lat']) + '°',
+                "longitude": str(retrieved_data['coord']['lon']) + '°',
+                "temp": str(round(retrieved_data['main']['temp'])) + ' °C',
+                "pressure": str(retrieved_data['main']['pressure']) + " hPa",
+                "humidity": str(retrieved_data['main']['humidity']) + '%',
+                'main': str(retrieved_data['weather'][0]['main']),
+                'description': str(retrieved_data['weather'][0]['description']),
+                'icon': retrieved_data['weather'][0]['icon'],
             }
+
         except KeyError:
             # If the city is not found, display the errors below
             data = {
